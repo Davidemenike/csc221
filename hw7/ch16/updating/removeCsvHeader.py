df4c722eb2f74c446199174611539ec87bcccd08
Davidemenike <59932349+Davidemenike@users.noreply.github.com>
2020-11-16 17:23:02 -0600

    Add files via upload

---
+++ hw7/ch16/updating/removeCsvHeader.py
@@ -0,0 +1,34 @@
+# Removes the header from all CSV files in the current working directory
+
+
+
+import csv, os
+
+os.makedirs('headerRemoved', exist_ok=True)
+
+# Loop through every file in the current working directory
+for csvFilename in os.listdir('.'):
+    if not csvFilename.endswith('.csv'):
+        continue  # which skips non-csv files
+
+
+    print('Removing header from' + csvFilename + '...')
+
+
+# Read the CSV file in (skipping first row).
+csvRows = []
+csvFileObj = open(csvFilename)
+readerObj = csv.reader(csvFileObj)
+for row in readerObj:
+    if readerObj.line_num == 1:
+        continue  # skip first row
+    csvRows.append(row)
+csvFileObj.close()
+
+
+# write out the CSV file.
+csvFileObj = open(os.path.join('headerRemoved', csvFilename), 'w', newline='')
+csvWriter = csv.writer(csvFileObj)
+for row in csvRows:
+    csvWriter.writerow(row)
+csvFileObj.close()
